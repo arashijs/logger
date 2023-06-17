017d9d6a1f52895f0647325f90b6b5b3e80c42c1
Norman Breau <norman@nbsolutions.ca>
2023-06-16 22:34:06 -0300

    expiremental: move cloudwatch usage to a different thread

--- src/CloudWatchStream.ts
+++ src/CloudWatchStream.ts
@@ -1,12 +1,14 @@
 
 import {Writable, WritableOptions} from 'stream';
+import {Worker} from 'worker_threads';
 import {ILogObject} from './ILogObject';
 import {
-    CloudWatchLogsClient,
-    CloudWatchLogsClientConfig,
-    PutLogEventsCommand,
-    PutLogEventsCommandOutput
+    // CloudWatchLogsClient,
+    CloudWatchLogsClientConfig
+    // PutLogEventsCommand,
+    // PutLogEventsCommandOutput
 } from '@aws-sdk/client-cloudwatch-logs';
+import EventEmitter = require('events');
 
 export interface ICWStreamConfig {
     group: string;
@@ -14,10 +16,20 @@ export interface ICWStreamConfig {
 }
 
 export class CloudWatchStream extends Writable {
-    private $client: CloudWatchLogsClient;
+    // private $client: CloudWatchLogsClient;
     private $streamConfig: ICWStreamConfig;
+    private $thread: Worker;
+    private $threadChannel: MessageChannel;
+    private $threadPort: MessagePort;
 
-    public constructor(awsConfig: CloudWatchLogsClientConfig, streamConfig: ICWStreamConfig, opts?: WritableOptions) {
+    public static async  create(awsConfig: CloudWatchLogsClientConfig, streamConfig: ICWStreamConfig, opts?: WritableOptions): Promise<CloudWatchStream> {
+        let stream: CloudWatchStream = new CloudWatchStream(streamConfig, opts);
+        await stream.$initThread(awsConfig);
+
+        return stream;
+    }
+
+    private constructor(streamConfig: ICWStreamConfig, opts?: WritableOptions) {
         super({
             ...opts,
             highWaterMark: 256,
@@ -25,47 +37,80 @@ export class CloudWatchStream extends Writable {
         });
 
         this.$streamConfig = streamConfig;
-        this.$client = new CloudWatchLogsClient(awsConfig);
+        // this.$client = new CloudWatchLogsClient(awsConfig);
+    }
+
+    private $initThread(awsConfig: CloudWatchLogsClientConfig): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this.$threadChannel = new MessageChannel();
+
+            let isTS: boolean = /\.ts$/.test(__filename);
+            this.$thread = new Worker(`./CloudWatchThread.${isTS ? 'ts' : 'js'}`, {
+                workerData: {
+                    awsConfig: awsConfig,
+                    streamConfig: this.$streamConfig,
+                    port: this.$threadChannel.port2
+                },
+                // This was tested and works, and is recommended by NodeJS docs,
+                // in fact if you don't transfer the port, using a port in workerData will produce
+                // an error
+                transferList: [ <any> this.$threadChannel.port2 ]
+            });
+
+            // We must wait for the thread to initiate and give us it's message port
+            // NodeJS version of MessagePort is indeed an EventEmitter...
+            (<EventEmitter><unknown> this.$threadChannel.port1).once('message', (msg: unknown) => {
+                if (!(msg instanceof MessagePort)) {
+                    reject(new Error('Expected MessagePort'));
+                    return;
+                }
+
+                this.$threadPort = msg;
+                resolve();
+            });
+        });
     }
 
     public override _write(chunk: ILogObject, encoding: BufferEncoding, callback: (error?: Error) => void): void {
-        let json: string;
-        try {
-            json = JSON.stringify(chunk);
-        }
-        catch (ex) {
-            callback(ex);
-            return;
-        }
+        this.$threadPort.postMessage(chunk);
+        callback();
+        // let json: string;
+        // try {
+        //     json = JSON.stringify(chunk);
+        // }
+        // catch (ex) {
+        //     callback(ex);
+        //     return;
+        // }
         
-        let comm: PutLogEventsCommand
-        try {
-            comm = new PutLogEventsCommand({
-                logGroupName: this.$streamConfig.group,
-                logStreamName: this.$streamConfig.stream,
-                logEvents: [
-                    {
-                        timestamp: chunk.timestamp,
-                        message: json
-                    }
-                ]
-            });
-        }
-        catch (ex) {
-            callback(ex);
-            return;
-        }
+        // let comm: PutLogEventsCommand
+        // try {
+        //     comm = new PutLogEventsCommand({
+        //         logGroupName: this.$streamConfig.group,
+        //         logStreamName: this.$streamConfig.stream,
+        //         logEvents: [
+        //             {
+        //                 timestamp: chunk.timestamp,
+        //                 message: json
+        //             }
+        //         ]
+        //     });
+        // }
+        // catch (ex) {
+        //     callback(ex);
+        //     return;
+        // }
 
-        try {
-            this.$client.send(comm).then((resp: PutLogEventsCommandOutput) => {
-                callback();
-            }).catch((error) => {
-                callback(error);
-            });
-        }
-        catch (ex) {
-            callback(ex);
-            return;
-        }
+        // try {
+        //     this.$client.send(comm).then((resp: PutLogEventsCommandOutput) => {
+        //         callback();
+        //     }).catch((error) => {
+        //         callback(error);
+        //     });
+        // }
+        // catch (ex) {
+        //     callback(ex);
+        //     return;
+        // }
     }
 }
---
+++ src/CloudWatchThread.ts
@@ -0,0 +1,60 @@
+
+import {ILogObject} from './ILogObject';
+import {EventEmitter} from 'events';
+import {
+    CloudWatchLogsClient,
+    PutLogEventsCommand,
+    PutLogEventsCommandOutput
+} from '@aws-sdk/client-cloudwatch-logs';
+import {workerData} from 'worker_threads';
+import { ICWStreamConfig } from './CloudWatchStream';
+
+let streamConfig: ICWStreamConfig = workerData.streamConfig;
+
+let client: CloudWatchLogsClient = new CloudWatchLogsClient(workerData.awsConfig);
+
+let channel: MessageChannel = new MessageChannel();
+
+// NodeJS version of MessagePort is indeed an EventEmitter...
+(<EventEmitter><unknown>channel.port1).on('message', (chunk: ILogObject) => {
+    let json: string;
+    try {
+        json = JSON.stringify(chunk);
+    }
+    catch (ex) {
+        console.error('CloudWatchThread', ex);
+        return;
+    }
+    
+    let comm: PutLogEventsCommand
+    try {
+        comm = new PutLogEventsCommand({
+            logGroupName: streamConfig.group,
+            logStreamName: streamConfig.stream,
+            logEvents: [
+                {
+                    timestamp: chunk.timestamp,
+                    message: json
+                }
+            ]
+        });
+    }
+    catch (ex) {
+        console.error('CloudWatchThread', ex);
+        return;
+    }
+
+    try {
+        client.send(comm).then((resp: PutLogEventsCommandOutput) => {
+            // yay
+        }).catch((error) => {
+            console.error('CloudWatchThread', error);
+        });
+    }
+    catch (ex) {
+        console.error('CloudWatchThread', ex);
+        return;
+    }
+});
+
+
